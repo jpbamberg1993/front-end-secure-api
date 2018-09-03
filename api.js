e7f2fab2cff458f7cc55a1c587bf5fd2c32c9a5a
John Paul Bamberg <jpbamberg1993@gmail.com>
2018-09-02 20:50:38 -0400

    Create public and private api endpoints

---
+++ api.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressJwt = require('express-jwt')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.API_PORT || 8888
+
+app.use(bodyParser())
+app.use(cors())
+
+app.get('/api/public', (req, res) => {
+  res.status(200)
+     .send('This is public content')
+})
+
+app.get(
+  '/api/private',
+  expressJwt({secret: 'mysupersecretkey'}),
+  (req, res) => {
+    res.status(200)
+       .send('This is private information')
+  })
+
+app.get('*', (req, res) => res.status(404))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))

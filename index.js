6f9540dbed4b0b187099aa7780828071c6b3837c
John Paul Bamberg <jpbamberg1993@gmail.com>
2018-09-09 15:49:07 -0400

    Enable login from UI

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const API_URL = 'http://localhost:3000'
 const AUTH_URL = 'http://localhost:8888'
 
+let ACCESS_TOKEN = undefined
+
 const headlineBtn = document.querySelector('#headline')
 const secretBtn = document.querySelector('#secret')
 const loginBtn = document.querySelector('#login')
 const logoutBtn = document.querySelector('#logout')
+
+loginBtn.addEventListener('click', (event) => {
+  modal.style.display = 'none'
+  fetch(`${AUTH_URL}/login`, {
+    method: 'POST',
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+      "accept": "application/json"
+    },
+    body: JSON.stringify(UIUpdate.getUsernamePassword())
+  }).then(resp => {
+    UIUpdate.alertCat(resp.status)
+    if (resp.status == 200) {
+      return resp.json()
+    } else {
+      return resp.text()
+    }
+  }).then(data => {
+    if (data.access_token) {
+      ACCESS_TOKEN = data.access_token
+      data = `Access Token: ${data.access_token}`
+      UIUpdate.loggedIn()
+    }
+    UIUpdate.alertBox(data)
+  })
+})
 
 headlineBtn.addEventListener('click', () => {
   fetch(`${API_URL}/api/public`)
--- ui-update.js
+++ ui-update.js
@@ -4,3 +4,20 @@ UIUpdate.alertBox = function(message) {
   const alertBox = document.querySelector('.alert')
   alertBox.innerHTML = message
 }
+
+UIUpdate.alertCat = function(status) {
+  const catBox = document.querySelector('#httpcat')
+  catBox.src = `http://http.cat/${status}`
+}
+
+UIUpdate.getUsernamePassword = function() {
+  return {
+    username: document.querySelector('#username').value,
+    password: document.querySelector('#password').value
+  }
+}
+
+UIUpdate.loggedIn = function(token) {
+  loginModalBtn.classList.add('d-none')
+  logoutBtn.classList.remove('d-none')
+}

9b7108f972236c78d2ad5290d2ae3a4841025bae
John Paul Bamberg <jpbamberg1993@gmail.com>
2018-09-09 15:44:43 -0400

    Remove unnecessary log

--- modal.js
+++ modal.js
@@ -3,7 +3,6 @@ const loginModalBtn = document.querySelector('#loginModalBtn')
 const closeBtn = document.querySelector('.close')
 
 loginModalBtn.addEventListener('click', function() {
-  console.log({loginModalBtn})
   modal.style.display = 'block'
 })
 
